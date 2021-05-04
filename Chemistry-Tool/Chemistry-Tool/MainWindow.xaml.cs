a39fddefa88ccf1a4485e05c2d0e24e89b476b1a
somecollagist <75587365+somecollagist@users.noreply.github.com>
2021-05-04 23:06:22 +0100

    Bit more UI and some functionality

---
+++ Chemistry-Tool/Chemistry-Tool/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Chemistry_Tool
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void AppLoader(object sender, RoutedEventArgs e)
+        {
+            AppData.Visibility = Visibility.Visible;
+            AppName.Text = ((Button)sender).Content.ToString();
+        }
+
+        public void LaunchApp(object sender, RoutedEventArgs e)
+        {
+            Window opener;
+            switch(AppName.Text)
+            {
+                case "Reversible Reaction Tool":
+                    opener = new ReversibleReactionTool();
+                    break;
+
+                default:
+                    throw new NotImplementedException($"Specified tool of name \"{AppName.Text}\" has not yet been implemented.");
+            }
+
+            opener.Show();
+        }
+
+        public void EH_About(object sender, RoutedEventArgs e)
+        {
+            About about = new About();
+            about.Show();
+        }
+
+        public void EH_OpenSettings(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public void EH_GoToWebsite(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://www.proleindustries.com");
+        }
+
+    }
+}
---
+++ Chemistry-Tool/Chemistry-Tool/ReversibleReactionTool.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Chemistry_Tool
+{
+    /// <summary>
+    /// Interaction logic for ReversibleReactionTool.xaml
+    /// </summary>
+    public partial class ReversibleReactionTool : Window
+    {
+        public ReversibleReactionTool()
+        {
+            InitializeComponent();
+        }
+
+        public void AddReactant(object sender, RoutedEventArgs e)
+        {
+            Reactants.Children.Add(GenericChemical());
+        }
+
+        public void AddProduct(object sender, RoutedEventArgs e)
+        {
+            Products.Children.Add(GenericChemical());
+        }
+
+        private UIElement GenericChemical()
+        {
+            StackPanel g = new StackPanel
+            {
+                Background = new SolidColorBrush(Color.FromArgb(0x44, 0x99, 0x99, 0x99)),
+                Margin = new Thickness(0, 5, 0, 5)
+            };
+
+            TextBox name = new TextBox
+            {
+                ToolTip = "Name of Chemical"
+            };
+
+            TextBox conc = new TextBox
+            {
+                ToolTip = "Concentration of Chemical (mol dm⁻³)",
+            };
+            conc.PreviewTextInput += new TextCompositionEventHandler(IsConcValid);
+
+            g.Children.Add(name);
+            g.Children.Add(conc);
+            return g;
+        }
+
+        private void IsConcValid(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex(@"^(\d+)((.)?(\d+))?$", RegexOptions.Multiline);
+            e.Handled = regex.IsMatch(e.Text);
+        }
+    }
+}
