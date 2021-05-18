d9ff4405cce19bc3de774bb3d529d196543f9d96
Johann Scott <75587365+somecollagist@users.noreply.github.com>
2021-05-18 17:33:37 +0100

    Begun work on Gauss-Jordan Elimination to balance chemical equations

--- Chemistry-Tool/Chemistry-Tool/EquationBalancer.xaml.cs
+++ Chemistry-Tool/Chemistry-Tool/EquationBalancer.xaml.cs
@@ -24,9 +24,27 @@ namespace Chemistry_Tool
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Generates a group of controls which allows a user to enter a chemical formula
+        /// </summary>
+        /// <returns>A grid control group for chemical formula, moles, and concentration</returns>
+        private static UIElement Unit()
+        {
+            //Build a parent stackpanel to contain the controls
+            StackPanel g = new StackPanel
+            {
+                Background = new SolidColorBrush(Color.FromArgb(0x44, 0x99, 0x99, 0x99)),
+                Margin = new Thickness(0, 5, 0, 5)
+            };
+
+            g.Children.Add(new TextBlock() { Style = (Style)Application.Current.FindResource("GenericTextBox"), Text = "Chemical Formula:" });  //Label for chemical formula
+            g.Children.Add(new TextBox() { ToolTip = "Formula of Chemical" });                                                                  //Entry for chemical formula                                                      //Entry for moles
+            return g;
+        }
+
         public void AddReactant(object sender, RoutedEventArgs e)
         {
-            Reactants.Children.Add(App.GenericChemical());
+            Reactants.Children.Add(Unit());
         }
 
         public void RemoveReactant(object sender, RoutedEventArgs e)
@@ -36,7 +54,7 @@ namespace Chemistry_Tool
 
         public void AddProduct(object sender, RoutedEventArgs e)
         {
-            Products.Children.Add(App.GenericChemical());
+            Products.Children.Add(Unit());
         }
 
         public void RemoveProduct(object sender, RoutedEventArgs e)
@@ -46,35 +64,55 @@ namespace Chemistry_Tool
 
         public void Balance(object sender, RoutedEventArgs e)
         {
-            Dictionary<Atom, int> ReactantAtoms = new Dictionary<Atom, int>();
-            Dictionary<Atom, int> ProductAtoms = new Dictionary<Atom, int>();
-
             if (Products.Children.Count * Reactants.Children.Count == 0 || Products.Children.Count * Reactants.Children.Count == 1)
             {
                 App.Alert("A total of at least 3 Products and Reactants must be specified");
                 return;
             }
 
-            foreach (Substance reactant in Reactants.Children.OfType<StackPanel>().Select(t => new Substance(t)))
+            //Implement a Gauss-Jordan Elimination to solve the chemical equation
+            Dictionary<Atom, int[]> GaussJordanMatrix = new Dictionary<Atom, int[]>();
+            List<Chemical> Chemicals = new List<Chemical>();
+            foreach (Chemical chemical in Reactants.Children.OfType<StackPanel>().Select(t => new Chemical(((TextBox)t.Children[1]).Text)))
             {
-                foreach(KeyValuePair<Atom, int> atoms in reactant.Chemical.Elements)
+                Chemicals.Add(chemical);
+            }
+            int FirstProductIndex = Chemicals.Count;
+            foreach (Chemical chemical in Products.Children.OfType<StackPanel>().Select(t => new Chemical(((TextBox)t.Children[1]).Text)))
+            {
+                Chemicals.Add(chemical);
+            }
+
+            for (int x = 0; x < Chemicals.Count; x++)
+            {
+                bool IsProduct = FirstProductIndex <= x;
+                foreach(KeyValuePair<Atom, int> kvp in Chemicals[x].Elements)
                 {
-                    if (!ReactantAtoms.ContainsKey(atoms.Key)) ReactantAtoms.Add(atoms.Key, atoms.Value);
-                    else ReactantAtoms[atoms.Key] += atoms.Value;
+                    Atom atom = kvp.Key; int AtomSigma = kvp.Value;
+                    if(!GaussJordanMatrix.ContainsKey(atom))
+                    {
+                        GaussJordanMatrix.Add(atom, new int[Chemicals.Count]);
+                    }
+
+                    AtomSigma -= Convert.ToInt16(IsProduct) * 2 * AtomSigma;
+                    GaussJordanMatrix[atom][x] = AtomSigma;
                 }
             }
 
-            foreach (Substance product in Products.Children.OfType<StackPanel>().Select(t => new Substance(t)))
-            {
-                foreach (KeyValuePair<Atom, int> atoms in product.Chemical.Elements)
-                {
-                    if (!ProductAtoms.ContainsKey(atoms.Key)) ProductAtoms.Add(atoms.Key, atoms.Value);
-                    else ProductAtoms[atoms.Key] += atoms.Value;
-                }
-            }
+            //ShowMatrix(GaussJordanMatrix);
+        }
 
-            double[] _reactants = ReactantAtoms.OrderBy(t => t.Key.AtomicNumber).Select(t => (double)t.Value).ToArray();
-            double[] _products  = ProductAtoms.OrderBy(t => t.Key.AtomicNumber).Select(t => (double)t.Value).ToArray();
+        private void ShowMatrix(Dictionary<Atom, int[]> matrix)
+        {
+            foreach(KeyValuePair<Atom, int[]> kvp in matrix)
+            {
+                Console.Write($"{kvp.Key.Symbol} >>> ");
+                foreach(int x in kvp.Value)
+                {
+                    Console.Write($"{x} ");
+                }
+                Console.Write("\n");
+            }
         }
     }
 }
