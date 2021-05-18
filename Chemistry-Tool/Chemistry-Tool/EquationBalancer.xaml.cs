3a4615eee8a912348bd18c5caa7d9462a7b34779
somecollagist <75587365+somecollagist@users.noreply.github.com>
2021-05-18 22:10:54 +0100

    Balancing almost works, but breaks sometimes in the Gauss-Jordan Elimination

--- Chemistry-Tool/Chemistry-Tool/EquationBalancer.xaml.cs
+++ Chemistry-Tool/Chemistry-Tool/EquationBalancer.xaml.cs
@@ -70,14 +70,19 @@ namespace Chemistry_Tool
                 return;
             }
 
+            BalancedEq.Visibility = Visibility.Hidden;
+
             //Implement a Gauss-Jordan Elimination to solve the chemical equation
-            Dictionary<Atom, int[]> GaussJordanMatrix = new Dictionary<Atom, int[]>();
-            List<Chemical> Chemicals = new List<Chemical>();
+            //See more here: http://www.ijstr.org/final-print/jan2020/-Application-Of-Gauss-jordan-Elimination-Method-In-Balancing-Typical-Chemical-Equations.pdf
+
+            Dictionary<Atom, double[]> GaussJordanMatrix = new Dictionary<Atom, double[]>();    //Will be used to find the ratios of all chemicals, atom key served to give ratio
+            List<Chemical> Chemicals = new List<Chemical>();                                    //List of all chemicals (used as a proxy to build the Matrix)
+
             foreach (Chemical chemical in Reactants.Children.OfType<StackPanel>().Select(t => new Chemical(((TextBox)t.Children[1]).Text)))
             {
                 Chemicals.Add(chemical);
             }
-            int FirstProductIndex = Chemicals.Count;
+            int FirstProductIndex = Chemicals.Count;    //Keep track of first product index, as the products must be input as negative (helps code run branchless)
             foreach (Chemical chemical in Products.Children.OfType<StackPanel>().Select(t => new Chemical(((TextBox)t.Children[1]).Text)))
             {
                 Chemicals.Add(chemical);
@@ -85,34 +90,120 @@ namespace Chemistry_Tool
 
             for (int x = 0; x < Chemicals.Count; x++)
             {
-                bool IsProduct = FirstProductIndex <= x;
+                bool IsProduct = FirstProductIndex <= x;    //Flag if the current chemical is a product or not
                 foreach(KeyValuePair<Atom, int> kvp in Chemicals[x].Elements)
                 {
-                    Atom atom = kvp.Key; int AtomSigma = kvp.Value;
+                    Atom atom = kvp.Key; int AtomSigma = kvp.Value; //Current atom being checked, amount of that atom in the chemical
                     if(!GaussJordanMatrix.ContainsKey(atom))
                     {
-                        GaussJordanMatrix.Add(atom, new int[Chemicals.Count]);
+                        //If the atom isn't in the matrix, add it in
+                        //Add 1 extra column to act as a pre-emptive matrix augmentation (column has to be 0)
+                        GaussJordanMatrix.Add(atom, new double[Chemicals.Count+1]);
                     }
 
-                    AtomSigma -= Convert.ToInt16(IsProduct) * 2 * AtomSigma;
-                    GaussJordanMatrix[atom][x] = AtomSigma;
+                    AtomSigma -= Convert.ToInt16(IsProduct) * 2 * AtomSigma;    //IsProduct*2 allows to make AtomSigma negative without branching
+                    GaussJordanMatrix[atom][x] = AtomSigma;                     //Sets the matrix value for that atom at that chemical index to be AtomSigma
                 }
             }
 
-            //ShowMatrix(GaussJordanMatrix);
+            //At this point, GaussJordanMatrix is a standard augmented matrix, it must now be converted to its reduced row echelon form (rref)
+            //This is how I'm doing it: https://www.geeksforgeeks.org/program-for-gauss-jordan-elimination-method/
+            //Because this is copy-paste I'm not commenting, so trust the player
+
+            //Convert to a 2d array because it makes things so much easier ygm
+            double[,] Matrix = new double[GaussJordanMatrix.Count, GaussJordanMatrix.Values.ToArray()[0].Length];
+            for(int x = 0; x < GaussJordanMatrix.Count; x++)
+            {
+                for(int y = 0; y < GaussJordanMatrix.Values.ToArray()[x].Length; y++)
+                {
+                    Matrix[x, y] = GaussJordanMatrix.Values.ToArray()[x][y];
+                }
+            }
+
+            int i, j, c;
+            int k = 0;
+            int n = GaussJordanMatrix.Count;
+
+            for(i = 0; i < n; i++)
+            {
+                if(Matrix[i,i] == 0)
+                {
+                    c = 1;
+                    while ((i + c) < n && Matrix[i + c, i] == 0) c++;
+                    if ((i + c) == n) break;
+                    for (j = i, k = 0; k <= n; k++)
+                    {
+                        double temp = Matrix[j, k];
+                        Matrix[j, k] = Matrix[j + c, k];
+                        Matrix[j + c, k] = temp;
+                    }
+                }
+                for (j = 0; j < n; j++)
+                {
+                    //Excluding all i == j
+                    if (i != j)
+                    {
+                        //Converts Matrix to ref
+                        double p = Matrix[j, i] / Matrix[i, i];
+                        for(k = 0; k <= n; k++) Matrix[j, k] = Matrix[j, k] - (Matrix[i, k]) * p;
+                    }
+                }
+            }
+            for (i = 0; i < n; i++)
+            {
+                double div = Matrix[i, i];
+                for (j = i; j < GaussJordanMatrix.Values.ToArray()[0].Length; j++) Matrix[i, j] /= div;
+            }
+            //Matrix is now in reduced row echelon form
+
+            //Make coefficients integers
+            double[] icoefficients = Enumerable.Range(0, Matrix.GetLength(0)).Select(x => Matrix[x, n]).ToArray();
+            double[] dcoefficients = new double[n + 1];
+            int[] coefficients = new int[n + 1];
+
+            double smallest = icoefficients.Select(t => t).Max();    //Min in abs, but max in real (since all are negative)
+            dcoefficients[n] = -1;
+            icoefficients.CopyTo(dcoefficients, 0);
+            dcoefficients = dcoefficients.Select(t => t * 1000 / smallest).ToArray();   //Multiply by 1000 to be REALLY safe over purging decimals
+            coefficients = dcoefficients.Select(t => (int)Math.Round(t)).ToArray();     //Haha floating points go brrr
+            int gcd = ArrayGCD(coefficients);
+            coefficients = coefficients.Select(t => t / gcd).ToArray();                 //Divide all the values by the greated common factor to give equation in simplest form
+            //Coefficients are fully balanced (apply to each chemical sequentially)
+
+            //Builds the equation in a nice string
+            List<string> coefchems = new List<string>();
+            for(int x = 0; x < Chemicals.Count; x++)
+            {
+                string cf = coefficients[x] == 1 ? "" : coefficients[x].ToString();
+                string push = $"{cf}{Chemicals[x].MolecularFormulaPretty}";
+                if (x == FirstProductIndex - 1) //To avoid spaghetti code, we add an arrow and wang in the next chemical all as one element (it's easier when joining)
+                {
+                    x++;
+                    cf = coefficients[x] == 1 ? "" : coefficients[x].ToString();
+                    push += $" → {cf}{Chemicals[x].MolecularFormulaPretty}";
+                }
+                coefchems.Add(push);
+            }
+
+            BalancedEq.Text = string.Join(" + ", coefchems);    //Add + signs between all elements
+            BalancedEq.Visibility = Visibility.Visible;
         }
 
-        private void ShowMatrix(Dictionary<Atom, int[]> matrix)
+        private int ArrayGCD(int[] arr)
         {
-            foreach(KeyValuePair<Atom, int[]> kvp in matrix)
+            int GCD(int a, int b)
             {
-                Console.Write($"{kvp.Key.Symbol} >>> ");
-                foreach(int x in kvp.Value)
-                {
-                    Console.Write($"{x} ");
-                }
-                Console.Write("\n");
+                if (a == 0) return b;
+                return GCD(b % a, a);
             }
+
+            int result = arr[0];
+            for(int i = 1; i < arr.Length; i++)
+            {
+                result = GCD(arr[i], result);
+                if (result == 1) return 1;
+            }
+            return result;
         }
     }
 }
